5e357c6331d4bc45b35ebf7e5c0c8ea899cebad6
Vojtěch Moravec <vojtisko83@seznam.cz>
2018-01-01 21:09:40 +0100

    Created xml scene "Idea"....

--- PrinciplesOfCG/XmlScene.h
+++ PrinciplesOfCG/XmlScene.h
@@ -1,10 +1,30 @@
 #pragma once
 #include "pugixml.hpp"
+#include "Application.h"
+#include "ObjectFactory.h"
+
+
 
 class XmlScene
 {
+private:
+    //pugi::xml_document document;
+    const std::string OBJECT_NODE = "Object";
+    const std::string CAMERA_NODE = "Camera";
+    const std::string LIGHT_NODE = "Light";
+    const std::string SHADER_NODE = "Shader";
+    const std::string COLLECTION_SUFFIX = "_set";
+
+    
+
+    Scene * scene = NULL;
 public:
     XmlScene();
     ~XmlScene();
+
+    Scene * loadScene(const char* xmlSceneFile);
+    bool saveScene(const Scene scene);
+
+    
 };
 
